711273cbf72a0a06b9f0418cf2a5d4c97686cb2f
SebastianHinz73 <76681744+SebastianHinz73@users.noreply.github.com>
2025-06-01 14:46:59 +0200

    commit vue conflicted changes

--- webapp/src/views/ShellyAdminView.vue
+++ webapp/src/views/ShellyAdminView.vue
@@ -5,30 +5,18 @@
         </BootstrapAlert>
 
         <form @submit="saveShellyConfig">
+
             <CardElement :text="$t('shellyadmin.ShellyGet')" textVariant="text-bg-primary">
-                <InputElement
-                    :label="$t('shellyadmin.ShellyGetSwitch')"
-                    v-model="shellyConfigList.shelly_enable"
-                    type="checkbox"
-                />
+                <InputElement :label="$t('shellyadmin.ShellyGetSwitch')" v-model="shellyConfigList.shelly_enable"
+                    type="checkbox" />
 
-                <InputElement
-                    :label="$t('shellyadmin.ShellyPro3em')"
-                    v-model="shellyConfigList.shelly_hostname_pro3em"
-                    type="text"
-                    maxlength="128"
-                    :placeholder="$t('shellyadmin.HostnameHint')"
-                    v-show="shellyConfigList.shelly_enable"
-                />
+                <InputElement :label="$t('shellyadmin.ShellyPro3em')" v-model="shellyConfigList.shelly_hostname_pro3em"
+                    type="text" maxlength="128" :placeholder="$t('shellyadmin.HostnameHint')"
+                    v-show="shellyConfigList.shelly_enable" />
 
-                <InputElement
-                    :label="$t('shellyadmin.ShellyPlugS')"
-                    v-model="shellyConfigList.shelly_hostname_plugs"
-                    type="text"
-                    maxlength="128"
-                    :placeholder="$t('shellyadmin.HostnameHint')"
-                    v-show="shellyConfigList.shelly_enable"
-                />
+                <InputElement :label="$t('shellyadmin.ShellyPlugS')" v-model="shellyConfigList.shelly_hostname_plugs"
+                    type="text" maxlength="128" :placeholder="$t('shellyadmin.HostnameHint')"
+                    v-show="shellyConfigList.shelly_enable" />
 
                 <InputElement :label="$t('shellyadmin.ShellyMoreInfoSwitch')" type="noinput">
                     <select class="form-select" v-model="shellyConfigList.view_option">
@@ -37,50 +25,31 @@
                         </option>
                     </select>
                 </InputElement>
+
+                <InputElement :label="$t('fileadmin.Download')" type="noinput">
+                    <a href="#" class="icon" :title="$t('fileadmin.Download')">
+                                    <BIconDownload v-on:click="downloadFile('shelly_data.bin')" />
+                                </a>
+                </InputElement>
             </CardElement>
 
-            <CardElement
-                :text="$t('shellyadmin.ShellySet')"
-                textVariant="text-bg-primary"
-                add-space
-                v-show="shellyConfigList.shelly_enable"
-            >
-                <InputElement
-                    :label="$t('shellyadmin.ShellySetSwitch')"
-                    v-model="shellyConfigList.limit_enable"
-                    type="checkbox"
-                    v-show="shellyConfigList.shelly_enable"
-                />
+            <CardElement :text="$t('shellyadmin.ShellySet')" textVariant="text-bg-primary" add-space
+                v-show="shellyConfigList.shelly_enable">
 
-                <InputElement
-                    :label="$t('shellyadmin.MaxPower')"
-                    v-model="shellyConfigList.max_power"
-                    type="number"
-                    min="1"
-                    max="3000"
-                    :placeholder="$t('shellyadmin.MaxPowerHint')"
-                    v-show="shellyConfigList.shelly_enable && shellyConfigList.limit_enable"
-                />
+                <InputElement :label="$t('shellyadmin.ShellySetSwitch')" v-model="shellyConfigList.limit_enable"
+                    type="checkbox" v-show="shellyConfigList.shelly_enable" />
 
-                <InputElement
-                    :label="$t('shellyadmin.MinPower')"
-                    v-model="shellyConfigList.min_power"
-                    type="number"
-                    min="0"
-                    max="500"
-                    :placeholder="$t('shellyadmin.MinPowerHint')"
-                    v-show="shellyConfigList.shelly_enable && shellyConfigList.limit_enable"
-                />
+                <InputElement :label="$t('shellyadmin.MaxPower')" v-model="shellyConfigList.max_power" type="number"
+                    min="1" max="3000" :placeholder="$t('shellyadmin.MaxPowerHint')"
+                    v-show="shellyConfigList.shelly_enable && shellyConfigList.limit_enable" />
 
-                <InputElement
-                    :label="$t('shellyadmin.TargetValue')"
-                    v-model="shellyConfigList.target_value"
-                    type="number"
-                    min="-100"
-                    max="300"
-                    :placeholder="$t('shellyadmin.TargetValueHint')"
-                    v-show="shellyConfigList.shelly_enable && shellyConfigList.limit_enable"
-                />
+                <InputElement :label="$t('shellyadmin.MinPower')" v-model="shellyConfigList.min_power" type="number"
+                    min="0" max="500" :placeholder="$t('shellyadmin.MinPowerHint')"
+                    v-show="shellyConfigList.shelly_enable && shellyConfigList.limit_enable" />
+
+                <InputElement :label="$t('shellyadmin.TargetValue')" v-model="shellyConfigList.target_value"
+                    type="number" min="-100" max="300" :placeholder="$t('shellyadmin.TargetValueHint')"
+                    v-show="shellyConfigList.shelly_enable && shellyConfigList.limit_enable" />
 
                 <div class="row mb-3" v-if="shellyConfigList.limit_enable">
                     <label for="inputFeedInLevel" class="col-sm-2 col-form-label">
@@ -89,21 +58,15 @@
                     </label>
                     <div class="col-sm-10">
                         <div class="input-group mb-3">
-                            <input
-                                type="range"
-                                class="form-control form-range"
-                                v-model="shellyConfigList.feed_in_level"
-                                min="0"
-                                max="100"
-                                id="inputFeedInLevel"
-                                aria-describedby="basic-addon1"
-                                style="height: unset"
-                            />
+                            <input type="range" class="form-control form-range" v-model="shellyConfigList.feed_in_level"
+                                min="0" max="100" id="inputFeedInLevel" aria-describedby="basic-addon1"
+                                style="height: unset;" />
                             <span class="input-group-text" id="basic-addon1">{{ FeedInLevelText }}</span>
                         </div>
                     </div>
                 </div>
             </CardElement>
+
             <FormFooter @reload="getShellyConfig" />
         </form>
     </BasePage>
@@ -111,13 +74,13 @@
 
 <script lang="ts">
 import BasePage from '@/components/BasePage.vue';
-import BootstrapAlert from '@/components/BootstrapAlert.vue';
+import BootstrapAlert from "@/components/BootstrapAlert.vue";
 import CardElement from '@/components/CardElement.vue';
 import FormFooter from '@/components/FormFooter.vue';
 import InputElement from '@/components/InputElement.vue';
-import type { ShellyConfig } from '@/types/ShellyConfig';
+import type { ShellyConfig } from "@/types/ShellyConfig";
 import { authHeader, handleResponse } from '@/utils/authentication';
-import { BIconInfoCircle } from 'bootstrap-icons-vue';
+import { BIconInfoCircle, BIconDownload } from 'bootstrap-icons-vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -128,13 +91,14 @@ export default defineComponent({
         FormFooter,
         InputElement,
         BIconInfoCircle,
+        BIconDownload,
     },
     data() {
         return {
             dataLoading: true,
             shellyConfigList: {} as ShellyConfig,
-            alertMessage: '',
-            alertType: 'info',
+            alertMessage: "",
+            alertType: "info",
             showAlert: false,
 
             viewOptionList: [
@@ -155,6 +119,7 @@ export default defineComponent({
                     descr: 'Complete Shelly info',
                 },
             ],
+
         };
     },
     created() {
@@ -162,35 +127,52 @@ export default defineComponent({
     },
     computed: {
         FeedInLevelText() {
-            return this.$t('shellyadmin.Percent', { per: this.$n(this.shellyConfigList.feed_in_level * 1) });
+            return this.$t("shellyadmin.Percent", { per: this.$n(this.shellyConfigList.feed_in_level * 1) });
         },
     },
     methods: {
         getShellyConfig() {
             this.dataLoading = true;
-            fetch('/api/shelly/config', { headers: authHeader() })
+            fetch("/api/shelly/config", { headers: authHeader() })
                 .then((response) => handleResponse(response, this.$emitter, this.$router))
-                .then((data) => {
-                    this.shellyConfigList = data;
-                    this.dataLoading = false;
-                });
+                .then(
+                    (data) => {
+                        this.shellyConfigList = data;
+                        this.dataLoading = false;
+                    }
+                );
         },
         saveShellyConfig(e: Event) {
             e.preventDefault();
 
             const formData = new FormData();
-            formData.append('data', JSON.stringify(this.shellyConfigList));
+            formData.append("data", JSON.stringify(this.shellyConfigList));
 
-            fetch('/api/shelly/config', {
-                method: 'POST',
+            fetch("/api/shelly/config", {
+                method: "POST",
                 headers: authHeader(),
                 body: formData,
             })
                 .then((response) => handleResponse(response, this.$emitter, this.$router))
-                .then((response) => {
-                    this.alertMessage = this.$t('apiresponse.' + response.code, response.param);
-                    this.alertType = response.type;
-                    this.showAlert = true;
+                .then(
+                    (response) => {
+                        this.alertMessage = this.$t('apiresponse.' + response.code, response.param);
+                        this.alertType = response.type;
+                        this.showAlert = true;
+                    }
+                );
+        },
+        downloadFile(filename: string) {
+            fetch('/api/shelly/file', { headers: authHeader() })
+                .then((res) => res.blob())
+                .then((blob) => {
+                    const file = window.URL.createObjectURL(blob);
+                    const a = document.createElement('a');
+                    a.href = file;
+                    a.download = filename;
+                    document.body.appendChild(a);
+                    a.click();
+                    a.remove();
                 });
         },
     },
